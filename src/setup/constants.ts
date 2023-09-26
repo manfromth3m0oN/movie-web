e2422d172d41a589c5fef42dce8e7e9fccf2915c
Max S-T <maxstantay@gmail.com>
2023-09-26 18:46:23 +0100

    change constants

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,6 +1,6 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
 export const DISCORD_LINK = "https://discord.gg/Jhqt4Xzpfb";
 export const GITHUB_LINK = "https://github.com/movie-web/movie-web";
-export const GA_ID = "G-44YVXRL61C";
+export const GA_ID = "G-KD8BVGX987";
 export const SENTRY_DSN =
-  "https://b267ab7d52674c23af4e4e6cf2956251@o4505053491167232.ingest.sentry.io/4505053495296000";
+  "https://6b7e37ffca729231320a08a8fbbd2d46@o4505948009594880.ingest.sentry.io/4505948011102208";
